6b232ee968bfadd44b9f48d38e5836060dc5de38
Alex <alex.leisure125@gmail.com>
2020-03-24 14:54:29 -0500

    added comments to factory methods and deleted a repeat method call in makeDeluxBurger()

--- DesignPatternsInCSharp/src/Factory/SpecialtyBurgerFactory.cs
+++ DesignPatternsInCSharp/src/Factory/SpecialtyBurgerFactory.cs
@@ -7,21 +7,25 @@ using DesignPatternsInCSharp.src.Builder;
 
 namespace DesignPatternsInCSharp.src.Factory {
 	class SpecialtyBurgerFactory {
+		//Makes a small cheese burger with ketchup and pickles
 		public static BurgerBuilder makeKidsCheeseBurger() {
 			BurgerBuilder b = new BurgerBuilder(Size.small);
 			b.AddCheese().AddKetchup().AddPickles();
 			return b;
 		}
 
+		//Makes a small burger with Ketchup and Pickles
 		public static BurgerBuilder makeKidsBurger() {
 			BurgerBuilder b = new BurgerBuilder(Size.small);
 			b.AddKetchup().AddPickles();
 			return b;
 		}
 
+		//Requires an input of size
+		//Makes a burger with cheese, lettuce, tomato, pickles, ketchup
 		public static BurgerBuilder makeDeluxeBurger(Size size) {
 			BurgerBuilder b = new BurgerBuilder(size);
-			b.AddCheese().AddLettuce().AddTomato().AddPickles().AddKetchup().AddTomato();
+			b.AddCheese().AddLettuce().AddTomato().AddPickles().AddKetchup();
 			return b;
 		}
 	}
